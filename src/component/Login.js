6b8626d285fec8cf4902cf410bafa636b949fb6e
Ike Njoku <ikeenjoku@gmail.com>
2018-12-25 00:59:23 +0100

    Remember login details

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,19 +1,75 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput, Keyboard, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput, NetInfo, ToastAndroid, TouchableOpacity } from 'react-native';
+import { CheckBox } from "react-native-elements";
 import { connect } from "react-redux";
+import { SecureStore } from "expo";
 import { register_page, loginUser } from "../redux/actions/authActions";
 import { add_alert } from "../redux/actions/alertActions";
 
 class Login extends Component {
   state = {
     email: '',
-    password: ''
+    password: '',
+    remember: false,
+  }
+
+  componentDidMount() {
+    SecureStore.getItemAsync('loginDetails')
+      .then((userdata) => {
+        let loginDetails = JSON.parse(userdata);
+        if (loginDetails) {
+          this.setState({ email: loginDetails.email });
+          this.setState({ password: loginDetails.password });
+          this.setState({ remember: true })
+        }
+      })
+      .catch(error => {
+        console.log('Error retrieving credentials')
+      });
+    NetInfo.getConnectionInfo()
+      .then((connectionInfo) => {
+        ToastAndroid.show('Initial Network Connectivity Type: '
+          + connectionInfo.type + ', effectiveType: ' + connectionInfo.effectiveType,
+          ToastAndroid.LONG)
+      });
+
+    NetInfo.addEventListener('connectionChange', this.handleConnectivityChange);
+  }
+
+  componentWillUnmount() {
+    NetInfo.removeEventListener('connectionChange', this.handleConnectivityChange);
+  }
+
+  handleConnectivityChange = (connectionInfo) => {
+    switch (connectionInfo.type) {
+      case 'none':
+        ToastAndroid.show('You are now offline!', ToastAndroid.LONG);
+        break;
+      case 'wifi':
+        ToastAndroid.show('You are now connected to WiFi!', ToastAndroid.LONG);
+        break;
+      case 'cellular':
+        ToastAndroid.show('You are now connected to Cellular!', ToastAndroid.LONG);
+        break;
+      case 'unknown':
+        ToastAndroid.show('You now have unknown connection!', ToastAndroid.LONG);
+        break;
+      default:
+        break;
+    }
   }
 
   handleLogIn = () => {
     const { email, password } = this.state;
-    if (email && password){
-    this.props.loginUser({ email, password });
+    if (email && password) {
+      this.props.loginUser({ email, password });
+      if (this.state.remember) {
+        SecureStore.setItemAsync('loginDetails', JSON.stringify({ email: this.state.email, password: this.state.password }))
+          .catch((error) => console.log('Could not save login info', error));
+      } else {
+        SecureStore.deleteItemAsync('loginDetails')
+          .catch((error) => console.log('Could not delete login info', error));
+      }
     } else {
       this.props.add_alert('Please fill in all fields');
     }
@@ -42,6 +98,14 @@ class Login extends Component {
               onChangeText={(text) => this.setState({ password: text })}
               value={this.state.password}
               style={styles.textInput} />
+          </View>
+          <View>
+            <CheckBox title="Remember Me"
+              center
+              checked={this.state.remember}
+              onPress={() => this.setState({ remember: !this.state.remember })}
+              containerStyle={styles.formCheckbox}
+            />
           </View>
           <View style={styles.buttonContainer}>
             <TouchableOpacity onPress={this.handleLogIn}>
@@ -124,7 +188,11 @@ const styles = StyleSheet.create({
   },
   formError: {
     color: 'red',
+  },
+  formCheckbox: {
+    backgroundColor: '#151F38',
+    borderColor: '#151F38'
   }
 });
 
-export default connect(null, {register_page, loginUser, add_alert})(Login);
+export default connect(null, { register_page, loginUser, add_alert })(Login);

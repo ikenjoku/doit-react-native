85f99cc4775dc73091c2e053d0ac4c4e138a51bb
Ike Njoku <ikeenjoku@gmail.com>
2018-12-25 01:30:22 +0100

    Remove redux persist

--- App.js
+++ App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
-import TodoApp from "./src/component/Todo";
-import Login from "./src/component/Login";
-import SignUp from "./src/component/SignUp";
 import Main from "./src/component/Main";
 import configureStore from './src/redux/configureStore';
-import { PersistGate } from 'redux-persist/es/integration/react'
-import { Loader } from "./src/component/Loader";
 
-// const { persistor, store } = configureStore();
+
 const store = configureStore();
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        {/* <PersistGate persistor={persistor} loading={<Loader />}> */}
           <Main />
-        {/* </PersistGate> */}
       </Provider>
     );
   }
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,26 +1,12 @@
-import {createStore, applyMiddleware, combineReducers} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import { persistStore, persistCombineReducers } from "redux-persist";
-import todoReducer from './reducers/todoReducer';
-import authReducer from './reducers/authReducer';
-import alertReducer from './reducers/alertReducer';
-import storage from 'redux-persist/es/storage';
 import rootReducer from './reducers';
-
-const config = {
-  key: 'root',
-  storage,
-  debug: true
-};
 
 const configureStore = () => {
   const store = createStore(
-    // persistCombineReducers(config, {
     rootReducer, applyMiddleware(thunk, logger));
 
-  // const persistor = persistStore(store);
-  // return { persistor, store };
   return store;
 };
 
